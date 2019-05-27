877274e8f88516cb9688ea2a1939ac7f7594bc1d
wenqi <guowenqi1983@hotmail.com>
2019-05-27 11:22:10 +0800

    Initial commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour{
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(obj.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            obj.transform.Translate(new Vector3(0, 0, 0.1f));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            obj.transform.Translate(new Vector3(0, 0, -0.1f));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            obj.transform.Translate(new Vector3(-0.1f, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            obj.transform.Translate(new Vector3(0.1f, 0, 0));
+        }
+    }
+}
